8e8bf4142b990118b874f639afc87c96c426a155
Mike <mike@tech.et>
2022-07-03 10:51:46 +0300

    animations

--- js/script.js
+++ js/script.js
@@ -1,6 +1,49 @@
-$(function() {
+$(function () {
   // jQuery goes here...
-
   // Uncomment this line to fade out the red box on page load
   // $(".red-box").fadeOut(2000);
+  // $(".green-box").fadeOut(2000);
+  // $(".red-box").fadeIn(2000);
+  // $(".red-box").fadeTo(2000, 0.2);
+  // $(".green-box").fadeTo(2000, 0.5);
+  // $(".blue-box").fadeTo(1000, 0.8);
+  // $(".green-box").fadeToggle();
+  // $(".green-box").fadeToggle();
+  // $(".green-box").toggle();
+  // $(".green-box").hide(1000);
+  // $(".green-box").show(1000);
+  // $(".blue-box").slideUp(2000);
+  // $(".blue-box").slideDown(2000);
+  // $("p").hide();
+  // $("p").slideDown(1000);
+  // $(".blue-box").animate(
+  //   {
+  //     marginLeft: "+=200px",
+  //     opacity: 0,
+  //     height: "50px",
+  //     with: "50px",
+  //     marginTop: "25px",
+  //   },
+  //   1000,
+  //   "linear"
+  // );
+  // $("p").animate(
+  //   {
+  //     fontSize: "20px",
+  //   },
+  //   1000
+  // );
+  //chaining functions
+  // $(".red-box").fadeTo(1000, 0.2);
+  // $(".green-box").delay(1000).fadeTo(1000, 0.5);
+  // $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(500).fadeIn();
+
+  //callback animation
+
+  // $(".red-box").fadeTo(1000, 0, function () {
+  //   alert("Hello, a callback animation");
+  // });
+
+  //fadein a lightbox signup
+  $(".lightbox").delay(500).fadeIn(1000);
 });
