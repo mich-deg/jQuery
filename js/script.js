fbb7392f7028db97cf7f483829f6e3c6acdc64d9
Mike <mike@tech.et>
2022-07-03 12:34:20 +0300

    CSS selectors with jQuery

--- js/script.js
+++ js/script.js
@@ -37,13 +37,40 @@ $(function () {
   // $(".red-box").fadeTo(1000, 0.2);
   // $(".green-box").delay(1000).fadeTo(1000, 0.5);
   // $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(500).fadeIn();
-
   //callback animation
-
   // $(".red-box").fadeTo(1000, 0, function () {
   //   alert("Hello, a callback animation");
   // });
-
   //fadein a lightbox signup
-  $(".lightbox").delay(500).fadeIn(1000);
+  // $(".lightbox").delay(500).fadeIn(1000);
+  //Using CSS selectors in jQuery
+  // $("#list").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("li:first").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("input:text").css("background-color", "rgb(180, 180, 30, 0.8)");
+  //JQuery Traversal
+  // $("#list").find("li").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("#list").children("li").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("#list").parents("div").css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("#list").parent().css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("#list")
+  //   .siblings(":header")
+  //   .css("background-color", "rgb(180, 180, 30, 0.8)");
+  //jQuery Filter
+  // $("#list")
+  //   .find("li")
+  //   .filter(":even")
+  //   .css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("li")
+  //   .filter(function (index) {
+  //     return index % 3 === 1;
+  //   })
+  //   .css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("li").first().css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("li").last().css("background-color", "rgb(180, 180, 30, 0.8)");
+  // $("li").eq(-2).css("background-color", "rgb(180, 180, 30, 0.8)");
+  $("li")
+    .not(function (index) {
+      return index % 3 === 2;
+    })
+    .css("background-color", "rgb(180, 180, 30, 0.8)");
 });
